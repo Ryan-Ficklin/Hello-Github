34eb851a7029122d363c769edc500479fba2c179
Ryan-Ficklin <ryanficklin@codelab-23.rockhurst.int>
2020-02-03 08:39:16 -0600

    Initial commit

---
+++ HelloGit.py
@@ -0,0 +1,19 @@
+# comment 
+a = 6
+b = 5
+print("Hello, World!")
+print(a+b)
+
+# how to make a list
+myList = [1, 2, 3]
+
+# how to access elements of a list 
+print(myList[1])
+
+# how to write a function
+def sumFunction(a, b):
+	return a + b
+
+# how to call a function
+print(sumFunction(2,20))
+
